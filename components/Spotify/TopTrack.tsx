7f368187738dd67446736ad59dcfab53c55c23ef
AshishVala <ashish.vala@itfaq.global>
2023-06-05 18:01:44 +0530

    changes

--- components/Spotify/TopTrack.tsx
+++ components/Spotify/TopTrack.tsx
@@ -10,14 +10,5 @@ export default function Tracks() {
     return null
   }
 
-  return (
-    <div className="py-7">
-      <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-        My <span className="text-green-700 dark:text-green-500">Spotify</span> Top Songs
-      </h1>
-      {topTracks.tracks.map((track, index) => (
-        <Track ranking={index + 1} key={track.songUrl} track={track} />
-      ))}
-    </div>
-  )
+  return <div></div>
 }
