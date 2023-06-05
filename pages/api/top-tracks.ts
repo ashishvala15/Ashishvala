af468b75258281a82a6a85d3265c2ad2103d221c
AshishVala <ashish.vala@itfaq.global>
2023-06-05 16:54:10 +0530

    changes

--- pages/api/top-tracks.ts
+++ pages/api/top-tracks.ts
@@ -11,14 +11,7 @@ export type Artist = {
 type Track = { artists: Artist[]; external_urls: { spotify: string }; name: string }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = await getTopTracks()
-  const { items } = await response.json()
-
-  const tracks = items.slice(0, 5).map((track: Track) => ({
-    artist: track.artists.map((_artist) => _artist.name).join(', '),
-    songUrl: track.external_urls.spotify,
-    title: track.name,
-  }))
+  const tracks = null
 
   res.setHeader('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=43200')
 
